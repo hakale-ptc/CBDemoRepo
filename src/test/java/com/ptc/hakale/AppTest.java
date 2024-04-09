afc6728dd5009758ccf3d903d127d0bfea345108
Harish Kale <147703879+hakale-ptc@users.noreply.github.com>
2024-04-09 15:04:58 -0400

    Add files via upload

---
+++ src/test/java/com/ptc/hakale/AppTest.java
@@ -0,0 +1,25 @@
+package com.ptc.hakale;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ * 
+ * update for demo #43334
+ */
+public class AppTest {
+    /**
+     * Rigorous Tests :-)
+     */
+    @Test
+    public void testThatShouldPass() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testThatShouldFail() {
+        assertTrue(false);
+    }
+}
